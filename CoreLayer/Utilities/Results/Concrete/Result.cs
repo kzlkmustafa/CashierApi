89c790df312aaadc12bd78616fb7795ec166b900
Mustafa Kızılkaya <kzlkmustafa@gmail.com>
2023-08-15 17:50:01 +0300

    Add Results

---
+++ CoreLayer/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,24 @@
+﻿using CoreLayer.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLayer.Utilities.Results.Concrete
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message): this(success)
+        {
+            MyMessage= message;
+        }
+        public Result(bool success)
+        {
+            IsSuccess= success;
+        }
+        public bool IsSuccess { get;}
+
+        public string MyMessage { get; }
+    }
+}
