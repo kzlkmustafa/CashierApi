ed0356a19e845a49d9ec1195d424b819d99a4829
Mustafa Kızılkaya <kzlkmustafa@gmail.com>
2023-08-15 18:21:04 +0300

    add genericService

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using CoreLayer.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<T>
+    {
+         Task<IDataResult<T>> GetById(int id); 
+        Task<IDataResult<IEnumerable<T>>> GetList();
+        Task<IResult> Add(T entity);
+        Task<IResult> Update(T entity);
+        Task<IResult> Delete(int id);
+    }
+}
