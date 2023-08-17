4967ce66212d2868ea729668c9d6c7be27649143
Mustafa Kızılkaya <kzlkmustafa@gmail.com>
2023-08-17 15:57:40 +0300

    added Cors

--- CashierApi/Program.cs
+++ CashierApi/Program.cs
@@ -2,6 +2,10 @@ using Autofac.Extensions.DependencyInjection;
 using Autofac;
 using BusinessLayer.DependencyResolves.Autofac;
 using BusinessLayer.Mapper.AutoMapper;
+using BusinessLayer.Security.Encyption;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using CoreLayer.Utilities.Security.jwt;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +24,34 @@ builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
     builder.RegisterModule(new AutofacBusinessModule());
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000");
+    });
+});
+
+var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(async options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidIssuer = tokenOptions.Issuer,
+        ValidAudience = tokenOptions.Audience,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = await SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+    };
+});
+
+
 var app = builder.Build();
+
+
+app.UseCors(builder => builder.WithOrigins("http://localhost:3000").AllowAnyHeader());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,6 +62,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
